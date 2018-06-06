a0e9d5fad53c06748a7bb566ffe0677cdd4edff0
Stephen Fry <sfry@financialforce.com>
2018-06-06 14:46:27 +0100

    Use more relaxed dependency versioning, update dependencies fix tests for new sinon.

--- src/spec/index/subscribe.spec.js
+++ src/spec/index/subscribe.spec.js
@@ -2,22 +2,22 @@
  * Copyright (c) 2017-2018 FinancialForce.com, inc
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, 
+ * Redistribution and use in source and binary forms, with or without modification,
  *   are permitted provided that the following conditions are met:
  *
- * - Redistributions of source code must retain the above copyright notice, 
+ * - Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, 
- *      this list of conditions and the following disclaimer in the documentation 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
  *      and/or other materials provided with the distribution.
- * - Neither the name of the FinancialForce.com, inc nor the names of its contributors 
- *      may be used to endorse or promote products derived from this software without 
+ * - Neither the name of the FinancialForce.com, inc nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
  *      specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
- *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
@@ -36,7 +36,6 @@ const
 
 	mocks = {},
 
-	sandbox = sinon.sandbox.create(),
 	expect = chai.expect,
 	anyFunction = sinon.match.func;
 
@@ -49,25 +48,25 @@ describe('index/subscribe.js', () => {
 
 	beforeEach(() => {
 		mocks.amqp = {
-			connect: sandbox.stub()
+			connect: sinon.stub()
 		};
 
 		mocks.connection = {
-			createChannel: sandbox.stub(),
-			close: sandbox.stub()
+			createChannel: sinon.stub(),
+			close: sinon.stub()
 		};
 
 		mocks.channel = {
-			assertQueue: sandbox.stub(),
-			prefetch: sandbox.stub(),
-			consume: sandbox.stub(),
-			ack: sandbox.stub()
+			assertQueue: sinon.stub(),
+			prefetch: sinon.stub(),
+			consume: sinon.stub(),
+			ack: sinon.stub()
 		};
 
 		mocks.amqp.connect.resolves(mocks.connection);
 		mocks.connection.createChannel.resolves(mocks.channel);
 
-		mocks.handler = sandbox.stub();
+		mocks.handler = sinon.stub();
 
 		subscribe = proxyquire(root + '/src/lib/index/subscribe', {
 			amqplib: mocks.amqp
@@ -75,7 +74,7 @@ describe('index/subscribe.js', () => {
 	});
 
 	afterEach(() => {
-		sandbox.restore();
+		sinon.restore();
 	});
 
 	describe('subscribe', () => {
